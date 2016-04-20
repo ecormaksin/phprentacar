5000d8f8a0dd276c6522bd7301189de72af847b9
MakotoOda <makotooda@debian6.0.10-01>
2016-04-20 22:13:24 +0900

    RservationControllerを作成し、予約フローのアクションとビューを追加

---
+++ src/Acme/RentacarBundle/Controller/ReservationController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Acme\RentacarBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * ReservationController.
+ *
+ * @author Your name <you@example.com>
+ *
+ * @Route("/reservation")
+ */
+class ReservationController extends AppController
+{
+	/**
+	 * @Route("/", name="reservation")
+	 * @Template
+	 */
+	public function indexAction(Request $request)
+	{
+		return array();
+	}
+
+	/**
+	 * @Route("/new", name="reservation_new")
+	 * @Template
+	 */
+	public function newAction(Request $request)
+	{
+		if ('POST' === $request->getMethod()) {
+			return $this->redirect($this->generateUrl('reservation_car'));
+		}
+
+		return array();
+	}
+
+	/**
+	 * @Route("/car", name="reservation_car")
+	 * @Template
+	 */
+	public function carAction(Request $request)
+	{
+		if ('POST' === $request->getMethod()) {
+			return $this->redirect($this->generateUrl('reservation_option'));
+		}
+
+		return array();
+	}
+
+	/**
+	 * @Route("/option", name="reservation_option")
+	 * @Template
+	 */
+	public function optionAction(Request $request)
+	{
+		if ('POST' === $request->getMethod() ) {
+			return $this->redirect($this->generateUrl('reservation_confirm'));
+		}
+
+		return array();
+	}
+
+	/**
+	 * @Route("/confirm", name="reservation_confirm")
+	 * @Template
+	 */
+	public function confirmAction()
+	{
+		return array();
+	}
+
+	/**
+	 * @Route("/finish", name="reservation_finish")
+	 * @Template
+	 */
+	public function finishAction(Request $request)
+	{
+		return array();
+	}
+}
