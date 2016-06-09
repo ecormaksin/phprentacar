e49ccc8d8a7f2c394404fb9ba48cd85ccf03664a
MakotoOda <makotooda@debian6.0.10-01>
2016-06-10 08:22:34 +0900

    車種一覧を作成

---
+++ src/Acme/RentacarBundle/Controller/CarClassController (コピー).php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Acme\RentacarBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * CarClassController.
+ *
+ * @author Your name <you@example.com>
+ *
+ * @Route("/car")
+ */
+class CarClassController extends AppController
+{
+    /**
+     * @Route("/", name="car_class")
+     */
+    public function indexAction()
+    {
+        $carClassRepository = $this->get('doctrine')->getRepository('AcmeRentacarBundle:CarClass');
+        $carClasses = $carClassRepository->findAll();
+
+        return array(
+            'carClasses' => $carClasses,
+        );
+    }
+}
