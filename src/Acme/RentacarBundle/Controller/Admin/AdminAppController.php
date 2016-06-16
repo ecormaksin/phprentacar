0a30406afb4f37543f1cd5185fb54be11c5f3d95
MakotoOda <makotooda@debian6.0.10-01>
2016-06-17 08:25:29 +0900

    予約一覧を作成

--- src/Acme/RentacarBundle/Controller/Admin/AdminAppController.php
+++ src/Acme/RentacarBundle/Controller/Admin/AdminAppController.php
@@ -6,7 +6,7 @@ use Crocos\SecurityBundle\Annotation\SecureConfig;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
- * @SeurityConfig(domain="admin", basic="admin:adminpass")
+ * @SecureConfig(domain="admin", basic="admin:adminpass")
  */
 abstract class AdminAppController extends Controller
 {
--- src/Acme/RentacarBundle/Controller/Admin/HomeController.php
+++ src/Acme/RentacarBundle/Controller/Admin/HomeController.php
@@ -4,7 +4,7 @@ namespace Acme\RentacarBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Conponent\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin")
